41be63ac347aeeb75be2f4cd948dd26718a4ef49
Renato <rnpenna@gmail.com>
2024-07-25 20:21:02 -0300

    ajustes 250720242020

--- resources/views/edicao_aluno.blade.php
+++ resources/views/edicao_aluno.blade.php
@@ -37,16 +37,16 @@
 		<div class="row alert alert-primary" role="alert"><div class="col-lg-12" align="center">Cadastro de Alunos</div></div>
         <p><div class="col-lg-12" align="right"><input type="button" class="btn btn-success" value="Listagem de Alunos" onclick="listagemAluno()"></div><br>
 		<div class="row">
-			<div class="col-lg-6"><label>Matrícula</label></div><div class="col-lg-6"><input type="text" id="matricula"  value="<?= $aluno->matricula ?>" readonly="readonly"></div>
+			<div class="col-lg-6"><label>Matrícula</label></div><div class="col-lg-6"><input type="text" name="matricula" id="matricula"  value="<?= $aluno->matricula ?>" readonly="readonly"></div>
 		</div>
 		<div class="row">
-        <div class="col-lg-3"><label>Nome Completo</label></div><div class="col-lg-3"><input type="text" id="nome" value="<?= $aluno->nome_completo ?>" required="required"></div>
-        <div class="col-lg-3"><label>Data de Nascimento</label></div><div class="col-lg-3"><input type="text" id="data_nascimento" value="<?= $aluno->data_nascimento ?>" required="required"></div>
+        <div class="col-lg-3"><label>Nome Completo</label></div><div class="col-lg-3"><input type="text" name="nome" id="nome" value="<?= $aluno->nome_completo ?>" required="required"></div>
+        <div class="col-lg-3"><label>Data de Nascimento</label></div><div class="col-lg-3"><input type="text" name="data_nascimento" id="data_nascimento" value="<?= $aluno->data_nascimento ?>" required="required"></div>
 		</div>
 
 		<div class="row">
 			<div class="col-lg-6"><label>Tipo de Endereço</label></div><div class="col-lg-6">
-                <select id="tipo_endereco" required="required">
+                <select name="tipo_endereco" id="tipo_endereco" required="required">
                     <option value="">Tipo de Endereço</option>
                     <option value="cobranca" <?php if($aluno->tipo_endereco == 'cobranca'){ echo 'selected'; } ?>>Cobrança</option>
                     <option value="residencial" <?php if($aluno->tipo_endereco == 'residencial'){ echo 'selected'; } ?>>Residencial</option>
@@ -56,43 +56,43 @@
 		</div>
 
 		<div class="row">
-        <div class="col-lg-3"><label>Rua</label></div><div class="col-lg-3"><input type="text" id="rua"  value="<?= $aluno->rua ?>" required="required"></div>
-        <div class="col-lg-3"><label>Cep</label></div><div class="col-lg-3"><input type="text" id="cep"  value="<?= $aluno->cep ?>" required="required"></div>
+        <div class="col-lg-3"><label>Rua</label></div><div class="col-lg-3"><input type="text" name="rua" id="rua"  value="<?= $aluno->rua ?>" required="required"></div>
+        <div class="col-lg-3"><label>Cep</label></div><div class="col-lg-3"><input type="text" name="cep" id="cep"  value="<?= $aluno->cep ?>" required="required"></div>
 		</div>
         
 		<div class="row">
-        <div class="col-lg-3"><label>Número</label></div><div class="col-lg-3"><input type="text" id="numero" value="<?= $aluno->numero ?>" required="required"></div>
-        <div class="col-lg-3"><label>Complemento</label></div><div class="col-lg-3"><input type="text" id="complemento" value="<?= $aluno->complemento ?>" required="required"></div>
+        <div class="col-lg-3"><label>Número</label></div><div class="col-lg-3"><input type="text" name="numero" id="numero" value="<?= $aluno->numero ?>" required="required"></div>
+        <div class="col-lg-3"><label>Complemento</label></div><div class="col-lg-3"><input type="text" name="numero" id="complemento" value="<?= $aluno->complemento ?>" required="required"></div>
 		</div>
 
 		<div class="row">
 			<div class="col-lg-3"><label>Série</label></div><div class="col-lg-3">
-                <select id="serie" required="required">
+                <select name="serie" id="serie" required="required">
                     <option value="">Escolha a Série abaixo</option>
-                    <option value="g1"<?php if($aluno->serie == 'g1'){ echo 'selected'; } ?>>G1</option>
-                    <option value="g2"<?php if($aluno->serie == 'g2'){ echo 'selected'; } ?>>G2</option>
-                    <option value="g3"<?php if($aluno->serie == 'g3'){ echo 'selected'; } ?>>G3</option>
-                    <option value="ano1"<?php if($aluno->serie == 'ano1'){ echo 'selected'; } ?>>1º ano</option>
-                    <option value="ano2"<?php if($aluno->serie == 'ano2'){ echo 'selected'; } ?>>2º ano</option>
-                    <option value="ano3"<?php if($aluno->serie == 'ano3'){ echo 'selected'; } ?>>3º ano</option>
-                    <option value="ano4"<?php if($aluno->serie == 'ano4'){ echo 'selected'; } ?>>4º ano</option>
-                    <option value="ano5"<?php if($aluno->serie == 'ano5'){ echo 'selected'; } ?>>5º ano</option>
-                    <option value="ano6"<?php if($aluno->serie == 'ano6'){ echo 'selected'; } ?>>6º ano</option>
-                    <option value="ano7"<?php if($aluno->serie == 'ano7'){ echo 'selected'; } ?>>7º ano</option>
-                    <option value="ano8"<?php if($aluno->serie == 'ano8'){ echo 'selected'; } ?>>8º ano</option>
-                    <option value="ano9"<?php if($aluno->serie == 'ano9'){ echo 'selected'; } ?>>9º ano</option>
-                    <option value="ano1EM"<?php if($aluno->serie == 'ano1EM'){ echo 'selected'; } ?>>1º ano do Ensino Médio</option>
-                    <option value="ano2EM"<?php if($aluno->serie == 'ano2EM'){ echo 'selected'; } ?>>2º ano do Ensino Médio</option>
-                    <option value="ano3EM"<?php if($aluno->serie == 'ano3EM'){ echo 'selected'; } ?>>3º ano do Ensino Médio</option>
+                    <option value="1"<?php if($turma->serie == '1'){ echo 'selected'; } ?>>G1</option>
+                    <option value="2"<?php if($turma->serie == '2'){ echo 'selected'; } ?>>G2</option>
+                    <option value="3"<?php if($turma->serie == '3'){ echo 'selected'; } ?>>G3</option>
+                    <option value="4"<?php if($turma->serie == '4'){ echo 'selected'; } ?>>1º ano</option>
+                    <option value="5"<?php if($turma->serie == '5'){ echo 'selected'; } ?>>2º ano</option>
+                    <option value="6"<?php if($turma->serie == '6'){ echo 'selected'; } ?>>3º ano</option>
+                    <option value="7"<?php if($turma->serie == '7'){ echo 'selected'; } ?>>4º ano</option>
+                    <option value="8"<?php if($turma->serie == '8'){ echo 'selected'; } ?>>5º ano</option>
+                    <option value="9"<?php if($turma->serie == '9'){ echo 'selected'; } ?>>6º ano</option>
+                    <option value="10"<?php if($turma->serie == '10'){ echo 'selected'; } ?>>7º ano</option>
+                    <option value="11"<?php if($turma->serie == '11'){ echo 'selected'; } ?>>8º ano</option>
+                    <option value="12"<?php if($turma->serie == '12'){ echo 'selected'; } ?>>9º ano</option>
+                    <option value="13"<?php if($turma->serie == '13'){ echo 'selected'; } ?>>1º ano do Ensino Médio</option>
+                    <option value="14"<?php if($turma->serie == '14'){ echo 'selected'; } ?>>2º ano do Ensino Médio</option>
+                    <option value="15"<?php if($turma->serie == '15'){ echo 'selected'; } ?>>3º ano do Ensino Médio</option>
                 </select>
             </div>
-			<div class="col-lg-3"><label>Segmento</label></div><div class="col-lg-3"><select id="segmento" value="<?= $aluno->segmento ?>" readonly="readonly"></select></div>
+			<div class="col-lg-3"><label>Segmento</label></div><div class="col-lg-3"><input type="text" name="segmento" id="segmento" value="<?= $aluno->segmento ?>" readonly="readonly"></div>
 		</div>
 
 
 		<div class="row">
-			<div class="col-lg-3"><label>Nome da mãe</label></div><div class="col-lg-3"><input type="text" id="nome_mae" required="required" value="<?= $aluno->nome_mae ?>"></div>
-			<div class="col-lg-3"><label>Nome do pai</label></div><div class="col-lg-3"><input type="text" id="nome_pai" required="required" value="<?= $aluno->nome_pai ?>"></div>
+			<div class="col-lg-3"><label>Nome da mãe</label></div><div class="col-lg-3"><input type="text" name="nome_mae" id="nome_mae" required="required" value="<?= $aluno->nome_mae ?>"></div>
+			<div class="col-lg-3"><label>Nome do pai</label></div><div class="col-lg-3"><input type="text" name="nome_pai" id="nome_pai" required="required" value="<?= $aluno->nome_pai ?>"></div>
 		</div>
 		<br>
 		<div class="col-lg-12" align="center">
--- resources/views/edicao_turma.blade.php
+++ resources/views/edicao_turma.blade.php
@@ -17,9 +17,9 @@
 		<div class="row alert alert-primary" role="alert"><div class="col-lg-12" align="center">Cadastro de Turma</div></div>
         <p><div class="col-lg-12" align="right"><input type="button" class="btn btn-success" value="Listagem de Turmas" onclick="listagemTurma()"></div><br>
 		<div class="row">
-        <div class="col-lg-3"><label>Nome da Turma</label></div><div class="col-lg-3"><input type="text" id="nome_turma" value="<?= $turma->nome_turma ?>" required="required"></div>
+        <div class="col-lg-3"><label>Nome da Turma</label></div><div class="col-lg-3"><input type="text" name="nome_turma" id="nome_turma" value="<?= $turma->nome_turma ?>" required="required"></div>
         <div class="col-lg-3"><label>Turno</label></div><div class="col-lg-3">
-            <select id="turno" required="required">
+            <select name="turno" id="turno" required="required">
                 <option value="">Selecione o Turno desejado</option>
                 <option value="manha"<?php if($turma->turno == 'manha'){ echo 'selected'; } ?>>Manhã</option>
                 <option value="tarde"<?php if($turma->turno == 'tarde'){ echo 'selected'; } ?>>Tarde</option>
@@ -28,32 +28,32 @@
         </div>
 
 		<div class="row">
-        <div class="col-lg-3"><label>Quantidade de Vagas</label></div><div class="col-lg-3"><input type="text" id="qtd_vagas" value="<?= $turma->qtd_vagas ?>" required="required"></div>
-        <div class="col-lg-3"><label>Ano letivo</label></div><div class="col-lg-3"><input type="text" id="ano" value="<?= $turma->ano ?>" required="required"></div>
+        <div class="col-lg-3"><label>Quantidade de Vagas</label></div><div class="col-lg-3"><input type="text" name="qtd_vagas" id="qtd_vagas" value="<?= $turma->qtd_vagas ?>" required="required"></div>
+        <div class="col-lg-3"><label>Ano letivo</label></div><div class="col-lg-3"><input type="text" name="ano_letivo" id="ano_letivo" value="<?= $turma->ano ?>" required="required"></div>
 		</div>
         
 		<div class="row">
 			<div class="col-lg-3"><label>Série</label></div><div class="col-lg-3">
-                <select id="serie" required="required">
+                <select name="serie" id="serie" required="required">
                     <option value="">Escolha a Série abaixo</option>
-                    <option value="g1"<?php if($turma->serie == 'g1'){ echo 'selected'; } ?>>G1</option>
-                    <option value="g2"<?php if($turma->serie == 'g2'){ echo 'selected'; } ?>>G2</option>
-                    <option value="g3"<?php if($turma->serie == 'g3'){ echo 'selected'; } ?>>G3</option>
-                    <option value="ano1"<?php if($turma->serie == 'ano1'){ echo 'selected'; } ?>>1º ano</option>
-                    <option value="ano2"<?php if($turma->serie == 'ano2'){ echo 'selected'; } ?>>2º ano</option>
-                    <option value="ano3"<?php if($turma->serie == 'ano3'){ echo 'selected'; } ?>>3º ano</option>
-                    <option value="ano4"<?php if($turma->serie == 'ano4'){ echo 'selected'; } ?>>4º ano</option>
-                    <option value="ano5"<?php if($turma->serie == 'ano5'){ echo 'selected'; } ?>>5º ano</option>
-                    <option value="ano6"<?php if($turma->serie == 'ano6'){ echo 'selected'; } ?>>6º ano</option>
-                    <option value="ano7"<?php if($turma->serie == 'ano7'){ echo 'selected'; } ?>>7º ano</option>
-                    <option value="ano8"<?php if($turma->serie == 'ano8'){ echo 'selected'; } ?>>8º ano</option>
-                    <option value="ano9"<?php if($turma->serie == 'ano9'){ echo 'selected'; } ?>>9º ano</option>
-                    <option value="ano1EM"<?php if($turma->serie == 'ano1EM'){ echo 'selected'; } ?>>1º ano do Ensino Médio</option>
-                    <option value="ano2EM"<?php if($turma->serie == 'ano2EM'){ echo 'selected'; } ?>>2º ano do Ensino Médio</option>
-                    <option value="ano3EM"<?php if($turma->serie == 'ano3EM'){ echo 'selected'; } ?>>3º ano do Ensino Médio</option>
+                    <option value="1"<?php if($turma->serie == '1'){ echo 'selected'; } ?>>G1</option>
+                    <option value="2"<?php if($turma->serie == '2'){ echo 'selected'; } ?>>G2</option>
+                    <option value="3"<?php if($turma->serie == '3'){ echo 'selected'; } ?>>G3</option>
+                    <option value="4"<?php if($turma->serie == '4'){ echo 'selected'; } ?>>1º ano</option>
+                    <option value="5"<?php if($turma->serie == '5'){ echo 'selected'; } ?>>2º ano</option>
+                    <option value="6"<?php if($turma->serie == '6'){ echo 'selected'; } ?>>3º ano</option>
+                    <option value="7"<?php if($turma->serie == '7'){ echo 'selected'; } ?>>4º ano</option>
+                    <option value="8"<?php if($turma->serie == '8'){ echo 'selected'; } ?>>5º ano</option>
+                    <option value="9"<?php if($turma->serie == '9'){ echo 'selected'; } ?>>6º ano</option>
+                    <option value="10"<?php if($turma->serie == '10'){ echo 'selected'; } ?>>7º ano</option>
+                    <option value="11"<?php if($turma->serie == '11'){ echo 'selected'; } ?>>8º ano</option>
+                    <option value="12"<?php if($turma->serie == '12'){ echo 'selected'; } ?>>9º ano</option>
+                    <option value="13"<?php if($turma->serie == '13'){ echo 'selected'; } ?>>1º ano do Ensino Médio</option>
+                    <option value="14"<?php if($turma->serie == '14'){ echo 'selected'; } ?>>2º ano do Ensino Médio</option>
+                    <option value="15"<?php if($turma->serie == '15'){ echo 'selected'; } ?>>3º ano do Ensino Médio</option>
                 </select>
             </div>
-			<div class="col-lg-3"><label>Segmento</label></div><div class="col-lg-3"><select id="segmento" readonly="readonly"></select></div>
+			<div class="col-lg-3"><label>Segmento</label></div><div class="col-lg-3"><input type="text" name="segmento" id="segmento" readonly="readonly"></div>
 		</div>
 		<br>
     		<div class="col-lg-12" align="center">
